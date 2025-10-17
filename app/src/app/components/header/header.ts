fd4d71c3cf104f656dbb3bd2f09ed6e5e0b8155e
krosbift <torifaoficial@gmail.com>
2025-10-17 16:59:59 -0500

    feat(component): catalog separated from header

--- app/src/app/components/header/header.ts
+++ app/src/app/components/header/header.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Search } from '../search/search';
+import { Catalog } from '../catalog/catalog';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
     Search,
+    Catalog,
   ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
