6c4ab4891f2221f9d64b382ab6ba4a3ba0435acb
krosbift <torifaoficial@gmail.com>
2025-10-17 21:00:42 -0500

    feat(save)

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { OurStores } from './pages/our-stores/our-stores';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   {
-    path: "**",
-    component: OurStores,
+    path: "",
+    component: Home,
   },
   {
     path: "nuestra-compañia/tiendas",
