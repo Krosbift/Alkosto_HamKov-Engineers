a7a6b4c3afb030ab5d083b6bb38c28b3949809fc
krosbift <torifaoficial@gmail.com>
2025-10-20 16:32:29 -0500

    feat(section-menu): se le agregaron animaciones y funcionalidad al boton de la sección del menu

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { OurStores } from './pages/our-stores/our-stores';
 import { Home } from './pages/home/home';
+import { LoginOptions } from './pages/login-options/login-options';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path: "nuestra-compañia/tiendas",
     component: OurStores,
-  }
+  },
+  {
+    path: "login/options",
+    component: LoginOptions,
+  },
 ];
