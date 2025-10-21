394e89225253ff16a031e3ef5060d177d9f1a12f
krosbift <torifaoficial@gmail.com>
2025-10-20 22:31:51 -0500

    faet(auth): base de estilos de autenticación desarrollada

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { OurStores } from './pages/our-stores/our-stores';
 import { Home } from './pages/home/home';
 import { LoginOptions } from './pages/login-options/login-options';
+import { Register } from './pages/register/register';
+import { EmailVerification } from './pages/email-verification/email-verification';
 
 export const routes: Routes = [
   {
@@ -15,5 +17,13 @@ export const routes: Routes = [
   {
     path: "login/options",
     component: LoginOptions,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'emailVerification',
+    component: EmailVerification,
   },
 ];
